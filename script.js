d5b081407dfe702313f1ba7b845d8461e1d5fa14
teekay24 <tkrafty24@gmail.com>
2024-12-18 20:30:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -164,10 +164,11 @@ document.addEventListener('DOMContentLoaded', () => {
     clearFiltersButton.addEventListener('click', clearTagAndPlayerSelections);
 
     // Handle player filter section toggle
-    const playerFilterHeader = document.querySelector('#player-filter h3');
-    const playerFilterSection = document.getElementById('player-filter');
-    playerFilterHeader.addEventListener('click', () => {
-        playerFilterSection.classList.toggle('open');
+    const togglePlayerFilterButton = document.getElementById('toggle-player-filter');
+    const playerFilterSection = document.getElementById('player-filter-section');
+    
+    togglePlayerFilterButton.addEventListener('click', () => {
+        playerFilterSection.style.display = (playerFilterSection.style.display === 'none') ? 'block' : 'none';
     });
 });
 
